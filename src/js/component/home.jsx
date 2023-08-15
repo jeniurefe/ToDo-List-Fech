2cf08b1e9e9d636bab0833a769ce161d72e87385
Jenipher Ureña Fdz <128754089+jeniurefe@users.noreply.github.com>
2023-08-15 22:11:34 +0000

    Integracion de API ToDos

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,123 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
+const urlBase = "https://playground.4geeks.com/apis/fake/todos/user/jeniurefe"
+const estadoIncial = {
+	label: "", done: false
+}
 //create your first component
 const Home = () => {
+	const [tarea, setTarea] = useState(estadoIncial)
+	const [lista, setLista] = useState([])
+const handleChange = (event) => {
+	setTarea({
+		...tarea, 
+		[event.target.name] : event.target.value 
+	})
+} 
+	const handleInput = async (e) => {
+		if (e.keyCode == 13) {
+			try {
+				let response = await fetch(urlBase,{
+					method: "PUT", 
+					headers: {
+						"Content-Type": "application/json"
+					} , 
+					body: JSON.stringify([...lista, tarea]) 
+				})
+				if(response.ok) {
+					getTask()
+				}
+			} catch (error) {
+				console.log(error)
+				
+			}
+		}
+	}
+
+	const deleteTask = async (index) => {
+		let tempArr = lista.slice() //copiar el estado lista en una variable auxiliar
+		tempArr = tempArr.filter((item, index2) => { return index2 != index })
+		try {
+			let response = await fetch(urlBase,{
+				method: "PUT", 
+				headers: {
+					"Content-Type": "application/json"
+				} , 
+				body: JSON.stringify(tempArr) 
+			})
+			if(response.ok) {
+				getTask()
+			}
+		} catch (error) {
+			console.log(error)
+			
+		}
+	}
+	const getTask = async() => {
+		try {
+			let response = await fetch(urlBase)
+			let data = await response.json()
+			if(response.ok){
+				setLista(data)
+			}
+			if(response.status == 404){
+				//crear un usuario
+				createUser()
+			}
+		} catch (error) {
+			console.log(error)
+			
+		}
+	}
+	const createUser = async() => {
+		try {
+			let response = await fetch(urlBase,{
+				method: "POST", 
+				headers: {
+					"Content-Type": "application/json"
+				} , 
+				body: JSON.stringify([]) 
+			})
+			if(response.ok) {
+				getTask()
+			}
+		} catch (error) {
+			console.log(error)
+			
+		}
+	}
+	useEffect(()=>{getTask()}, []) 
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div className="container text-center mt-5 caja ">
+				<div className="container text-center mt-5 display-5 todos">Todos</div>
+				<div className=" mt-5 text-center shadow p-3 mb-5 bg-body rounded ">
+					<input className="container border-top-0 border-light" placeholder="Agregar Tarea" name="label" value={tarea.label} onChange={handleChange}
+						onKeyUp={
+							(e) => { handleInput(e) }
+						} />
+					<div>
+						<ul className="list-group list-group-flush ">
+							{
+								lista && lista.length > 0 ?
+									<>{
+										lista.map((item, index) => {
+											return <li className="list-group-item border" key={index}>
+												{item.label}
+												<button type="button" className="btn btn-outline-light boton text-danger"onClick={e => { deleteTask(index) }}><i class="fa-solid fa-trash-can"></i>
+												</button>
+											</li>
+										})
+									}</>
+									: "Agregar tarea"
+							}
+						</ul>
+					</div>
+				</div>
+
+			</div>
+		</>
 	);
 };
 
